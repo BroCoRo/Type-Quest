491701ab0a6eda28bff6298dc97ee5a765d866ec
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2022-03-03 10:06:34 -0500

    Add project files.

---
+++ Team_name.h_Project_Section 3_Group 16/Source.c
@@ -0,0 +1,16 @@
+// CSCN71030 - Team Based Project Development 
+// Group Project Group 16 - Section 3
+// Raghu, Tyler, Owen, Brodin
+// March 3rd, 2022
+// 
+// Revision History 
+// 1.0       March 3rd      2022
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello world\n");
+
+	return(0);
+}
