ac1427230c87cc24661d1471409443c2c8039483
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2022-03-17 11:17:30 -0400

    Setup typing module files.

---
+++ Team_name.h_Project_Section 3_Group 16/Typing.c
@@ -0,0 +1,9 @@
+// CSCN71030 - Team Based Project Development 
+// Group Project Group 16 - Section 3
+// Brodin
+// March 17th, 2022
+// 
+// This is the typing module file that hold all of the functions relating to the typing module
+// 
+// Revision History 
+// 1.0       March 17th      2022
---
+++ Team_name.h_Project_Section 3_Group 16/Typing.h
@@ -0,0 +1,11 @@
+// CSCN71030 - Team Based Project Development 
+// Group Project Group 16 - Section 3
+// Brodin
+// March 17th, 2022
+// 
+// This is the header file for the typing module
+// 
+// Revision History 
+// 1.0       March 17th      2022
+
+#pragma once
