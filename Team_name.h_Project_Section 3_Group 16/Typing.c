9f009c2805f8d7b2cb28c1c3083ecf573b5f0d01
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2022-03-17 12:23:44 -0400

    DisplaySentence function written

--- Team_name.h_Project_Section 3_Group 16/Typing.c
+++ Team_name.h_Project_Section 3_Group 16/Typing.c
@@ -3,9 +3,31 @@
 // Brodin
 // March 17th, 2022
 // 
-// This is the typing module file that hold all of the functions relating to the typing module
+// This is the typing module file that holds all of the functions relating to the typing module
 // 
 // Revision History 
 // 1.0       March 17th      2022
 
 #include "Typing.h"
+
+//This function will output the sentence that the user must type to the terminal.
+char* DisplaySentence(char* monsterSentence)
+{
+	//print the sentence that the user is required to type to the screen
+	printf("%s\n", monsterSentence);
+	//return the sentence that the user must type
+	return monsterSentence;
+}
+
+//This function will get the input from the user (this input should match the sentence from the disaply sentence function 
+char* GetInput(int storeSpeed)
+{
+
+}
+
+//This function takes in the sentence that the user had to type, and it takes in the sentence that the user did type,
+//and it takes in the speed in which the user typed the sentence to find generated damage amount.
+double CheckSentence(char* sentenceToType, char* sentenceTyped, double typingSpeed)
+{
+
+}
--- Team_name.h_Project_Section 3_Group 16/Typing.h
+++ Team_name.h_Project_Section 3_Group 16/Typing.h
@@ -9,6 +9,7 @@
 // 1.0       March 17th      2022
 
 #pragma once
+#include <stdio.h>
 
 //This function will output the sentence that the user must type to the terminal.
 char* DisplaySentence(char* monsterSentence); 
@@ -18,4 +19,4 @@ char* GetInput(int storeSpeed);
 
 //This function takes in the sentence that the user had to type, and it takes in the sentence that the user did type,
 //and it takes in the speed in which the user typed the sentence to find generated damage amount.
-double CheckSentence(char*, char*, double);
+double CheckSentence(char* sentenceToType, char* sentenceTyped, double typingSpeed);
