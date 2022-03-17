ac1aa67aa9b37b3105e39c8b3ecdac6454634cea
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2022-03-17 12:13:44 -0400

    Typing score calculated with fast time test method written

--- TypingModuleTests/TypingModuleTests.cpp
+++ TypingModuleTests/TypingModuleTests.cpp
@@ -46,6 +46,12 @@ namespace TypingModuleTests
 		//This test method will test if the calcultated damadge from typing funtion works properly 
 		TEST_METHOD(TypingScoreCalculationFunctionality_INPUT_PerfectMatch_FastTime_PerfectScoreOUTPUTTED)
 		{
+			double calculatedScore;
+			double recordedSpeed = 2;
+			//calculate the score with an non-impactful time and a perfectly matched sentence
+			calculatedScore = CheckSentence("Test sentence", "Test sentence", recordedSpeed);
+			//check for perfect score
+			Assert::AreEqual(1.0, calculatedScore);
 		}
 		//This test method will test if the calcultated damadge from typing funtion works properly 
 		TEST_METHOD(TypingScoreCalculationFunctionality_INPUT_PerfectMatch_ModerateTime_ModerateScoreOUTPUTTED)
